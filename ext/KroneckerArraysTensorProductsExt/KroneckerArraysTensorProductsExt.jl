0baa17db9eb41ce79dbc74f3db92ef7a59b36fb5
mtfishman <mfishman@flatironinstitute.org>
2025-08-11 16:45:45 -0400

    KroneckerArraysTensorProductsExt

--- ext/KroneckerArraysTensorProductsExt/KroneckerArraysTensorProductsExt.jl
+++ ext/KroneckerArraysTensorProductsExt/KroneckerArraysTensorProductsExt.jl
@@ -1,11 +1,11 @@
 module KroneckerArraysTensorProductsExt
 
-using KroneckerArrays: CartesianProductOneTo, ×, arg1, arg2, cartesianrange
+using KroneckerArrays: CartesianProductOneTo, ×, arg1, arg2, cartesianrange, unproduct
 using TensorProducts: TensorProducts, tensor_product
 function TensorProducts.tensor_product(a1::CartesianProductOneTo, a2::CartesianProductOneTo)
-  return cartesianrange(
-    tensor_product(arg1(a1), arg1(a2)) × tensor_product(arg2(a1), arg2(a2))
-  )
+  prod = tensor_product(arg1(a1), arg1(a2)) × tensor_product(arg2(a1), arg2(a2))
+  range = tensor_product(unproduct(a1), unproduct(a2))
+  return cartesianrange(prod, range)
 end
 
 end
