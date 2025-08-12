2c2d41e4b0442c82bc02558b9dcad3c1f6010eda
Matt Fishman <mtfishman@users.noreply.github.com>
2025-08-11 23:11:50 -0400

    Update to BlockSparseArrays v0.9 (#36)

--- src/fillarrays/kroneckerarray.jl
+++ src/fillarrays/kroneckerarray.jl
@@ -41,24 +41,11 @@ function _convert(::Type{AbstractMatrix{T}}, a::RectDiagonal) where {T}
   RectDiagonal(convert(AbstractVector{T}, _diagview(a)), axes(a))
 end
 
-# Like `similar` but preserves `Eye`.
-function _similar(a::AbstractArray, elt::Type, ax::Tuple)
-  return similar(a, elt, ax)
+# Like `similar` but preserves `Eye`, `Ones`, etc.
+using FillArrays: Ones
+function _similar(arrayt::Type{<:Ones}, axs::Tuple)
+  return Ones{eltype(arrayt)}(axs)
 end
-function _similar(A::Type{<:AbstractArray}, ax::Tuple)
-  return similar(A, ax)
-end
-function _similar(a::AbstractArray, ax::Tuple)
-  return _similar(a, eltype(a), ax)
-end
-function _similar(a::AbstractArray, elt::Type)
-  return _similar(a, elt, axes(a))
-end
-function _similar(a::AbstractArray)
-  return _similar(a, eltype(a), axes(a))
-end
-
-# Like `similar` but preserves `Eye`.
 function _similar(a::Eye, elt::Type, axs::NTuple{2,AbstractUnitRange})
   return Eye{elt}(axs)
 end
@@ -76,19 +63,6 @@ end
 
 # Like `copy` but preserves `Eye`.
 _copy(a::Eye) = a
-
-using DerivableInterfaces: DerivableInterfaces, zero!
-function DerivableInterfaces.zero!(a::EyeKronecker)
-  zero!(a.b)
-  return a
-end
-function DerivableInterfaces.zero!(a::KroneckerEye)
-  zero!(a.a)
-  return a
-end
-function DerivableInterfaces.zero!(a::EyeEye)
-  return throw(ArgumentError("Can't zero out `Eye ⊗ Eye`."))
-end
 
 using Base.Broadcast:
   AbstractArrayStyle, AbstractArrayStyle, BroadcastStyle, Broadcasted, broadcasted
