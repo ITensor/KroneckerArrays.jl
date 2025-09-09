59dcc2810360d47dca6401fe52089ffaf5dbe11a
mtfishman <mfishman@flatironinstitute.org>
2025-09-09 16:54:18 -0400

    Mark broken inference

--- test/test_blocksparsearrays.jl
+++ test/test_blocksparsearrays.jl
@@ -9,7 +9,7 @@ using LinearAlgebra: norm
 using MatrixAlgebraKit: svd_compact, svd_trunc
 using StableRNGs: StableRNG
 using Test: @test, @test_broken, @testset
-using TestExtras: @constinferred
+using TestExtras: @constinferred, @constinferred_broken
 
 elts = (Float32, Float64, ComplexF32)
 arrayts = (Array, JLArray)
@@ -338,7 +338,12 @@ end
     Block(2, 2) => dev(δ(elt, (3, 3)) ⊗ randn(elt, 3, 3)),
   )
   a = dev(blocksparse(d, (r, r)))
-  @test @constinferred(norm(a)) ≈ norm(Array(a))
+  if VERSION ≥ v"1.11-"
+    @test @constinferred(norm(a)) ≈ norm(Array(a))
+  else
+    # Type inference fails in Julia 1.10.
+    @test @constinferred_broken(norm(a)) ≈ norm(Array(a))
+  end
 
   r = blockrange([2 × 2, 3 × 3])
   d = Dict(
