25eeb0a3e98c4f1e7b6aa4622445d91f93339f4b
Lukas Devos <ldevos98@gmail.com>
2025-11-11 20:21:00 -0500

    update the rest of the tests

--- test/test_tensoralgebra.jl
+++ test/test_tensoralgebra.jl
@@ -1,10 +1,10 @@
 using TensorAlgebra: matricize, unmatricize
-using KroneckerArrays: ⊗, arg1, arg2
+using KroneckerArrays: ⊗, kroneckerfactors
 using Test: @test, @testset
 
 @testset "TensorAlgebraExt" begin
     a = randn(2, 2, 2) ⊗ randn(3, 3, 3)
     m = matricize(a, (1, 2), (3,))
-    @test m == matricize(arg1(a), (1, 2), (3,)) ⊗ matricize(arg2(a), (1, 2), (3,))
+    @test m == matricize(kroneckerfactors(a, 1), (1, 2), (3,)) ⊗ matricize(kroneckerfactors(a, 2), (1, 2), (3,))
     @test unmatricize(m, (axes(a, 1), axes(a, 2)), (axes(a, 3),)) == a
 end
