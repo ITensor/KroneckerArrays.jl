f532818b93d7adcaddebb0df090f552ba0db990c
Matt Fishman <mtfishman@users.noreply.github.com>
2025-11-24 17:57:24 -0500

    Upgrade to TensorAlgebra v0.5 (#60)

--- ext/KroneckerArraysTensorAlgebraExt/KroneckerArraysTensorAlgebraExt.jl
+++ ext/KroneckerArraysTensorAlgebraExt/KroneckerArraysTensorAlgebraExt.jl
@@ -1,8 +1,8 @@
 module KroneckerArraysTensorAlgebraExt
 
 using KroneckerArrays: KroneckerArrays, AbstractKroneckerArray, ⊗, kroneckerfactors
-using TensorAlgebra: TensorAlgebra, AbstractBlockPermutation, BlockedTrivialPermutation, FusionStyle,
-    matricize, unmatricize
+using TensorAlgebra: TensorAlgebra, AbstractBlockPermutation, BlockedTrivialPermutation,
+    FusionStyle, matricize, unmatricize
 
 struct KroneckerFusion{A <: FusionStyle, B <: FusionStyle} <: FusionStyle
     a::A
@@ -11,32 +11,44 @@ end
 KroneckerArrays.kroneckerfactors(style::KroneckerFusion) = (style.a, style.b)
 KroneckerArrays.kroneckerfactortypes(::Type{KroneckerFusion{A, B}}) where {A, B} = (A, B)
 
-TensorAlgebra.FusionStyle(a::AbstractKroneckerArray) = KroneckerFusion(FusionStyle.(kroneckerfactors(a))...)
+function TensorAlgebra.FusionStyle(a::AbstractKroneckerArray)
+    return KroneckerFusion(FusionStyle.(kroneckerfactors(a))...)
+end
 function matricize_kronecker(
-        style::KroneckerFusion, a::AbstractArray, biperm::AbstractBlockPermutation{2}
+        style::FusionStyle, a::AbstractArray, length1::Val, length2::Val
     )
-    return matricize(kroneckerfactors(style, 1), kroneckerfactors(a, 1), biperm) ⊗
-        matricize(kroneckerfactors(style, 2), kroneckerfactors(a, 2), biperm)
+    m1 = matricize(kroneckerfactors(style, 1), kroneckerfactors(a, 1), length1, length2)
+    m2 = matricize(kroneckerfactors(style, 2), kroneckerfactors(a, 2), length1, length2)
+    return m1 ⊗ m2
 end
 function TensorAlgebra.matricize(
-        style::KroneckerFusion, a::AbstractArray, biperm::AbstractBlockPermutation{2}
+        style::KroneckerFusion, a::AbstractArray, length1::Val, length2::Val
     )
-    return matricize_kronecker(style, a, biperm)
+    return matricize_kronecker(style, a, length1, length2)
 end
-# Fix ambiguity error.
-# TODO: Investigate rewriting the logic in `TensorAlgebra.jl` to avoid this.
-using TensorAlgebra: BlockedTrivialPermutation, unmatricize
-function TensorAlgebra.matricize(
-        style::KroneckerFusion, a::AbstractArray, biperm::BlockedTrivialPermutation{2}
+function unmatricize_kronecker(
+        style::FusionStyle,
+        m::AbstractMatrix,
+        codomain_axes::Tuple{Vararg{AbstractUnitRange}},
+        domain_axes::Tuple{Vararg{AbstractUnitRange}},
     )
-    return matricize_kronecker(style, a, biperm)
+    style1, style2 = kroneckerfactors(style)
+    m1, m2 = kroneckerfactors(m)
+    codomain1 = kroneckerfactors.(codomain_axes, 1)
+    codomain2 = kroneckerfactors.(codomain_axes, 2)
+    domain1 = kroneckerfactors.(domain_axes, 1)
+    domain2 = kroneckerfactors.(domain_axes, 2)
+    a1 = unmatricize(style1, m1, codomain1, domain1)
+    a2 = unmatricize(style2, m2, codomain2, domain2)
+    return a1 ⊗ a2
 end
-function unmatricize_kronecker(style::KroneckerFusion, a::AbstractArray, ax)
-    return unmatricize(kroneckerfactors(style, 1), kroneckerfactors(a, 1), kroneckerfactors.(ax, 1)) ⊗
-        unmatricize(kroneckerfactors(style, 2), kroneckerfactors(a, 2), kroneckerfactors.(ax, 2))
-end
-function TensorAlgebra.unmatricize(style::KroneckerFusion, a::AbstractArray, ax)
-    return unmatricize_kronecker(style, a, ax)
+function TensorAlgebra.unmatricize(
+        style::KroneckerFusion,
+        m::AbstractMatrix,
+        codomain_axes::Tuple{Vararg{AbstractUnitRange}},
+        domain_axes::Tuple{Vararg{AbstractUnitRange}},
+    )
+    return unmatricize_kronecker(style, m, codomain_axes, domain_axes)
 end
 
 end
